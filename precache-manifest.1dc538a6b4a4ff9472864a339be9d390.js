a9f9e69ec706720dbd8a3c6ece36ac5e91d32ee8
Tibirius <tibirius@gmail.com>
2019-04-10 13:07:44 -0400

    Updates

---
+++ precache-manifest.1dc538a6b4a4ff9472864a339be9d390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "6b5b00678c8a215143fe",
+    "url": "/clicky/static/js/main.e5fe473e.chunk.js"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/clicky/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "6b5b00678c8a215143fe",
+    "url": "/clicky/static/css/main.d34c629f.chunk.css"
+  },
+  {
+    "revision": "b2adad9076c9e53a0a6b50353d6ccb7c",
+    "url": "/clicky/index.html"
+  }
+];
